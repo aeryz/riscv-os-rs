06b43a3bc3d2e72f7bb82aaeb4bd05ee1e2d5208
aeryz <abdullaheryz@protonmail.com>
2026-02-27 02:19:22 +0300

    add nix setup
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

---
+++ flake.nix
@@ -0,0 +1,87 @@
+{
+  description = "xv6-riscv kernel in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+      flake-utils.lib.eachSystem
+      (with flake-utils.lib.system; [ x86_64-linux ])
+      (system: 
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs { inherit system overlays; };
+          # bpf-linker has a hard requirement on llvm 21
+          llvmPackages = pkgs.llvmPackages_21;
+          # this specific rust version is built on llvm 21, DO NOT blindly upgrade
+          # or ebpf compilation will break
+          rust-toolchain = pkgs.rust-bin.nightly."2025-12-15".default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+            ];
+            targets = [
+              "riscv64gc-unknown-none-elf"
+            ];
+          };
+
+          rustfmt = pkgs.rust-bin.stable.latest.rustfmt;
+
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = rust-toolchain;
+            rustc = rust-toolchain;
+          };
+
+          bindgen = rustPlatform.buildRustPackage rec {
+            pname = "bindgen-cli";
+            version = "v0.72.1";
+            src = pkgs.fetchFromGitHub {
+              owner = "rust-lang";
+              repo = "rust-bindgen";
+              rev = "27577d2930af9311495b0d0f016f903824521ddc";
+              sha256 = "sha256-cswBbshxTAcZtUk3PxH9jD55X1a/fBAyZyYpzVkt27M=";
+            };
+
+            cargoLock = {
+              lockFile = "${src}/Cargo.lock";
+            };
+
+            nativeBuildInputs = [ pkgs.pkg-config ];
+
+            buildInputs = [ pkgs.openssl ];
+
+            doCheck = false;
+          };
+
+        riscv-toolchain =
+          import nixpkgs {
+            localSystem = "${system}";
+            crossSystem = {
+              config = "riscv64-none-elf";
+              libc = "newlib-nano";
+              abi = "ilp32";
+            };
+          };
+        in
+    {
+    devShells.default = pkgs.mkShell {
+      packages = with pkgs; [
+        clang
+        openssl
+        pkg-config
+      ] ++ [
+        rust-toolchain
+        bindgen
+        rustfmt
+        riscv-toolchain.buildPackages.binutils
+      ];
+
+      shellHook = ''
+        export LIBCLANG_PATH=${llvmPackages.libclang.lib}/lib
+      '';
+    };
+  });
+}
