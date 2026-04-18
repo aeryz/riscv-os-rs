7d017c92020787dcaf5d7b6af042d24ae25d6a32
aeryz <abdullaheryz@protonmail.com>
2026-04-18 22:29:39 +0300

    move sleeping tasks to the scheduler
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- kernel/src/task/table.rs
+++ kernel/src/task/table.rs
@@ -8,10 +8,6 @@ const MAX_TASK: usize = 32;
 
 static TASK_TABLE: TaskTable = TaskTable(SpinLock::new(heapless::Vec::new()));
 
-// TODO(aeryz): this sleep table can be smarter. Instead of pushing to the vec, we can
-// insert it in a sorted way and pop later.
-static SLEEP_TABLE: SleepTable = SleepTable(SpinLock::new(heapless::Vec::new()));
-
 pub fn add_task(task: Task) -> NonNull<Task> {
     let mut table = TASK_TABLE.0.lock();
     let len = table.len();
@@ -19,18 +15,6 @@ pub fn add_task(task: Task) -> NonNull<Task> {
 
     NonNull::new(&mut table[len] as *mut Task).expect("task is nonnull")
 }
-
-pub fn add_sleeping_task(task: NonNull<Task>) {
-    let mut table = SLEEP_TABLE.0.lock();
-    table
-        .push(task)
-        .expect("we ran out of slots in the task table");
-}
-
-struct SleepTable(SpinLock<heapless::Vec<NonNull<Task>, MAX_TASK>>);
-
-unsafe impl Send for SleepTable {}
-unsafe impl Sync for SleepTable {}
 
 struct TaskTable(SpinLock<heapless::Vec<Task, MAX_TASK>>);
 
