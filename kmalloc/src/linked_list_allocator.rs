b9c8f5f28479c250b19b38c2272cc6be1e8255e8
kernelnomad <kernelnomad@proton.me>
2026-04-23 22:29:42 +0300

    fix kmalloc tests
    Signed-off-by: kernelnomad <kernelnomad@proton.me>

--- kmalloc/src/linked_list_allocator.rs
+++ kmalloc/src/linked_list_allocator.rs
@@ -21,23 +21,6 @@ struct Header {
     next: Option<*mut Header>,
 }
 
-// Header
-// Padding
-// Back offset
-// Data
-
-// 2
-//
-// 100
-// H (132)
-// Data
-// 8 geldi
-// 132 + 32 = 164 % 8 != 0
-// 4 byte bos
-// H (190)
-// Data
-// o
-
 impl KernelAllocator for LinkedListAllocator {
     unsafe fn new(start_addr: usize, end_addr: usize) -> Result<Self, ()> {
         // The allocator should at least be able to fit a single header
@@ -58,7 +41,7 @@ impl KernelAllocator for LinkedListAllocator {
 }
 
 unsafe impl GlobalAlloc for LinkedListAllocator {
-    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let mut cur_node_ptr = self.head;
 
         loop {
@@ -74,30 +57,32 @@ unsafe impl GlobalAlloc for LinkedListAllocator {
 
             let alloc_size = back_offset + layout.size();
 
-            if cur_node.free && cur_node.sz > alloc_size {
+            if cur_node.free && cur_node.sz >= alloc_size {
                 let prev_sz = cur_node.sz;
-                cur_node.sz = alloc_size;
-                cur_node.free = false;
-
-                unsafe {
-                    *(back_offset_pos as *mut usize) = back_offset;
-                }
                 let remaining = prev_sz - alloc_size;
 
                 if remaining >= (size_of::<Header>() + size_of::<usize>()) {
-                    let next_node_ptr = unsafe { (cur_node as *mut Header).byte_add(cur_node.sz) };
+                    cur_node.sz = alloc_size;
+                    cur_node.free = false;
 
                     unsafe {
+                        *(back_offset_pos as *mut usize) = back_offset;
+
+                        let next_node_ptr =
+                            (cur_node_ptr as *mut u8).add(alloc_size) as *mut Header;
                         *next_node_ptr = Header {
                             sz: remaining,
                             free: true,
                             next: cur_node.next,
                         };
+                        cur_node.next = Some(next_node_ptr);
                     }
-
-                    cur_node.next = Some(next_node_ptr);
                 } else {
-                    cur_node.sz = prev_sz;
+                    cur_node.free = false;
+                    // consume whole block
+                    unsafe {
+                        *(back_offset_pos as *mut usize) = back_offset;
+                    }
                 }
 
                 return data_start as *mut u8;
@@ -110,7 +95,7 @@ unsafe impl GlobalAlloc for LinkedListAllocator {
         }
     }
 
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+    unsafe fn dealloc(&self, ptr: *mut u8, _: Layout) {
         let back_offset = unsafe {
             let back_offset_ptr = ptr.byte_sub(size_of::<usize>()).cast::<usize>();
             if back_offset_ptr.is_null() {
@@ -133,58 +118,106 @@ unsafe impl GlobalAlloc for LinkedListAllocator {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use core::{
+        alloc::{AllocError, Allocator, GlobalAlloc, Layout},
+        mem::align_of,
+        ptr::{self, NonNull},
+    };
+    use std::prelude::v1::*;
+    use std::{
+        collections::{BTreeMap, VecDeque},
+        format,
+    };
+
+    use crate::{KernelAllocator, LinkedListAllocator};
+
+    const HEAP_SIZE: usize = 1024 * 1024;
+
+    #[repr(align(4096))]
+    struct TestHeap([u8; HEAP_SIZE]);
+
+    static mut HEAP: TestHeap = TestHeap([0; HEAP_SIZE]);
+
+    unsafe impl Allocator for &LinkedListAllocator {
+        fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
+            let ptr = unsafe { GlobalAlloc::alloc(*self, layout) };
+            let nn = NonNull::new(ptr).ok_or(AllocError)?;
+            Ok(NonNull::slice_from_raw_parts(nn, layout.size()))
+        }
+
+        unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
+            unsafe { GlobalAlloc::dealloc(*self, ptr.as_ptr(), layout) }
+        }
+    }
+
+    #[repr(align(64))]
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    struct Align64(u8);
+
+    #[repr(align(256))]
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    struct Align256(u64);
 
     #[test]
-    fn malloc_free() {
-        let reserved_space = [0; 0x10000];
+    fn collections_can_use_linked_list_allocator_directly() {
+        let start = unsafe { HEAP.0.as_mut_ptr() as usize };
+        let end = start + HEAP_SIZE;
 
-        let start_addr = reserved_space.as_ptr() as usize;
-
-        let mut allocator = unsafe {
-            LinkedListAllocator::new(start_addr, start_addr + reserved_space.len()).unwrap()
+        let alloc = unsafe {
+            LinkedListAllocator::new(start, end).expect("failed to initialize LinkedListAllocator")
         };
 
-        // [ (H || 64) || .. ]
-        let a1 = allocator.allocate::<[u8; 64]>().unwrap();
-        assert_eq!(a1.as_ptr() as usize, start_addr + size_of::<Header>());
+        let mut bytes = Vec::new_in(&alloc);
+        for i in 0..4096 {
+            bytes.push((i % 251) as u8);
+        }
+        assert_eq!(bytes.len(), 4096);
+        assert_eq!(bytes[123], (123 % 251) as u8);
+        assert_eq!((bytes.as_ptr() as usize) % align_of::<u8>(), 0);
 
-        // [ (H || 64) || (H || 128) || .. ]
-        let a2 = allocator.allocate::<[u8; 128]>().unwrap();
-        assert_eq!(
-            a2.as_ptr() as usize,
-            a1.as_ptr() as usize + 64 + size_of::<Header>()
-        );
+        let mut a64 = Vec::new_in(&alloc);
+        for i in 0..128 {
+            a64.push(Align64(i as u8));
+        }
+        assert_eq!(a64[17], Align64(17));
+        assert_eq!((a64.as_ptr() as usize) % align_of::<Align64>(), 0);
 
-        // [ (H || 64) || (H || 128) || (H || 192) || .. ]
-        let a3 = allocator.allocate::<[u8; 192]>().unwrap();
-        assert_eq!(
-            a3.as_ptr() as usize,
-            a2.as_ptr() as usize + 128 + size_of::<Header>()
-        );
+        let mut a256 = Vec::new_in(&alloc);
+        for i in 0..32 {
+            a256.push(Align256(i as u64));
+        }
+        assert_eq!(a256[9], Align256(9));
+        assert_eq!((a256.as_ptr() as usize) % align_of::<Align256>(), 0);
 
-        // [ (H || 64) || (H || Free(128)) || (H || 192) || .. ]
-        allocator.free(a2).unwrap();
+        // VecDeque
+        let mut dq = VecDeque::new_in(&alloc);
+        for i in 0..1000 {
+            dq.push_back(i);
+        }
+        for i in 0..200 {
+            assert_eq!(dq.pop_front(), Some(i));
+        }
+        assert_eq!(dq.front(), Some(&200));
+        assert_eq!(dq.back(), Some(&999));
 
-        // [ (H || 64) || (H || 24) || (H || Free(104)) || (H || 192) || .. ]
-        let a5 = allocator.allocate::<[u8; 24]>().unwrap();
-        assert_eq!(
-            a5.as_ptr() as usize,
-            a1.as_ptr() as usize + 64 + size_of::<Header>()
-        );
+        let mut map = BTreeMap::new_in(&alloc);
+        for i in 0..500 {
+            map.insert(i, format!("value-{i}"));
+        }
+        assert_eq!(map.get(&77).map(String::as_str), Some("value-77"));
+        assert_eq!(map.get(&499).map(String::as_str), Some("value-499"));
 
-        // [ (H || 64) || (H || 24) || || (H || 32) || (H || Free(72)) || (H || 192) || .. ]
-        let a6 = allocator.allocate::<[u8; 32]>().unwrap();
-        assert_eq!(
-            a6.as_ptr() as usize,
-            a5.as_ptr() as usize + 24 + size_of::<Header>()
-        );
+        drop(bytes);
+        drop(a64);
+        drop(a256);
+        drop(dq);
+        drop(map);
 
-        // [ (H || 64) || (H || 24) || || (H || 32) || (H || Free(72)) || (H || 192) || (H || 96) ]
-        let a7 = allocator.allocate::<[u8; 96]>().unwrap();
-        assert_eq!(
-            a7.as_ptr() as usize,
-            a3.as_ptr() as usize + 192 + size_of::<Header>()
-        );
+        let mut after = Vec::new_in(&alloc);
+        for i in 0..256u128 {
+            after.push(i);
+        }
+        assert_eq!(after[255], 255);
+        assert_eq!((after.as_ptr() as usize) % align_of::<u128>(), 0);
     }
 }
