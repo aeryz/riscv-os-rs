9c7c08705738c53d9128846fb781f2429ac8ffe9
aeryz <abdullaheryz@protonmail.com>
2026-04-23 21:16:55 +0300

    kmalloc contd
    Signed-off-by: aeryz <abdullaheryz@protonmail.com>

--- kmalloc/src/lib.rs
+++ kmalloc/src/lib.rs
@@ -2,4 +2,23 @@
 
 mod linked_list_allocator;
 
+use core::alloc::GlobalAlloc;
+
 pub use linked_list_allocator::*;
+
+pub trait KernelAllocator: GlobalAlloc + Sized {
+    /// Creates a new allocator
+    ///
+    /// * `start_addr`: The start of the address that is reserved for this allocator.
+    /// * `end_addr`: The end of the address that is reserved for this allocator.
+    ///
+    /// ## Safety
+    /// - `start_addr` and `end_addr` are valid addresses during the execution of this allocator.
+    /// It's a really common mistake to initialize this allocator with physical addresses before
+    /// starting paging and then immediately get a trap once paging is enabled.
+    unsafe fn new(start_addr: usize, end_addr: usize) -> Result<Self, ()>;
+}
+
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
+}
--- kmalloc/src/linked_list_allocator.rs
+++ kmalloc/src/linked_list_allocator.rs
@@ -1,6 +1,11 @@
 //! ## Linked list allocator
 
-use core::ptr::NonNull;
+use core::{
+    alloc::{GlobalAlloc, Layout},
+    ptr::NonNull,
+};
+
+use crate::KernelAllocator;
 
 #[repr(C)]
 pub struct LinkedListAllocator {
@@ -16,17 +21,25 @@ struct Header {
     next: Option<*mut Header>,
 }
 
-impl LinkedListAllocator {
-    /// Creates a new allocator
-    ///
-    /// * `start_addr`: The start of the address that is reserved for this allocator.
-    /// * `end_addr`: The end of the address that is reserved for this allocator.
-    ///
-    /// ## Safety
-    /// - `start_addr` and `end_addr` are valid addresses during the execution of this allocator.
-    /// It's a really common mistake to initialize this allocator with physical addresses before
-    /// starting paging and then immediately get a trap once paging is enabled.
-    pub unsafe fn new(start_addr: usize, end_addr: usize) -> Result<Self, ()> {
+// Header
+// Padding
+// Back offset
+// Data
+
+// 2
+//
+// 100
+// H (132)
+// Data
+// 8 geldi
+// 132 + 32 = 164 % 8 != 0
+// 4 byte bos
+// H (190)
+// Data
+// o
+
+impl KernelAllocator for LinkedListAllocator {
+    unsafe fn new(start_addr: usize, end_addr: usize) -> Result<Self, ()> {
         // The allocator should at least be able to fit a single header
         if start_addr.checked_add(size_of::<Header>()).ok_or(())? > end_addr {
             return Err(());
@@ -36,41 +49,58 @@ impl LinkedListAllocator {
         head.sz = end_addr - start_addr;
         head.free = true;
         head.next = None;
-
         Ok(Self {
             start_addr,
             end_addr,
             head,
         })
     }
+}
 
-    /// Allocate `size_of::<T>()` bytes
-    pub fn alloc<T>(&mut self) -> Result<NonNull<T>, ()> {
+unsafe impl GlobalAlloc for LinkedListAllocator {
+    unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
         let mut cur_node_ptr = self.head;
-
-        let alloc_size = size_of::<T>().checked_add(size_of::<Header>()).ok_or(())?;
 
         loop {
             let cur_node = unsafe { cur_node_ptr.as_mut().unwrap() };
+
+            let header_start = cur_node_ptr as usize;
+            let data_start = crate::align_up(
+                header_start + size_of::<Header>() + size_of::<usize>(),
+                layout.align(),
+            );
+            let back_offset_pos = data_start - size_of::<usize>();
+            let back_offset = data_start - header_start;
+
+            let alloc_size = back_offset + layout.size();
+
             if cur_node.free && cur_node.sz > alloc_size {
                 let prev_sz = cur_node.sz;
                 cur_node.sz = alloc_size;
                 cur_node.free = false;
 
-                let next_node_ptr = unsafe { (cur_node as *mut Header).byte_add(cur_node.sz) };
                 unsafe {
-                    *next_node_ptr = Header {
-                        sz: prev_sz - cur_node.sz,
-                        free: true,
-                        next: cur_node.next,
-                    };
+                    *(back_offset_pos as *mut usize) = back_offset;
+                }
+                let remaining = prev_sz - alloc_size;
+
+                if remaining >= (size_of::<Header>() + size_of::<usize>()) {
+                    let next_node_ptr = unsafe { (cur_node as *mut Header).byte_add(cur_node.sz) };
+
+                    unsafe {
+                        *next_node_ptr = Header {
+                            sz: remaining,
+                            free: true,
+                            next: cur_node.next,
+                        };
+                    }
+
+                    cur_node.next = Some(next_node_ptr);
+                } else {
+                    cur_node.sz = prev_sz;
                 }
 
-                cur_node.next = Some(next_node_ptr);
-
-                return Ok(unsafe {
-                    NonNull::new_unchecked(cur_node_ptr.byte_add(size_of::<Header>()) as *mut T)
-                });
+                return data_start as *mut u8;
             }
 
             cur_node_ptr = match cur_node.next {
@@ -80,15 +110,24 @@ impl LinkedListAllocator {
         }
     }
 
-    /// Frees the allocated memoty for `ptr`.
-    ///
-    /// Note that this only labels the memory as freed. It does not zero out the storage.
-    ///
-    /// - `ptr`: The pointer to the allocated space initially acquired using [`alloc`];
-    pub fn free<T>(&mut self, ptr: NonNull<T>) -> Result<(), ()> {
-        let header = unsafe { ptr.byte_sub(size_of::<Header>()).cast::<Header>().as_mut() };
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        let back_offset = unsafe {
+            let back_offset_ptr = ptr.byte_sub(size_of::<usize>()).cast::<usize>();
+            if back_offset_ptr.is_null() {
+                return;
+            }
+
+            *back_offset_ptr
+        };
+
+        let header = unsafe {
+            match ptr.byte_sub(back_offset).cast::<Header>().as_mut() {
+                Some(ptr) => ptr,
+                None => return,
+            }
+        };
+
         header.free = true;
-        Ok(())
     }
 }
 
@@ -107,18 +146,18 @@ mod tests {
         };
 
         // [ (H || 64) || .. ]
-        let a1 = allocator.alloc::<[u8; 64]>().unwrap();
+        let a1 = allocator.allocate::<[u8; 64]>().unwrap();
         assert_eq!(a1.as_ptr() as usize, start_addr + size_of::<Header>());
 
         // [ (H || 64) || (H || 128) || .. ]
-        let a2 = allocator.alloc::<[u8; 128]>().unwrap();
+        let a2 = allocator.allocate::<[u8; 128]>().unwrap();
         assert_eq!(
             a2.as_ptr() as usize,
             a1.as_ptr() as usize + 64 + size_of::<Header>()
         );
 
         // [ (H || 64) || (H || 128) || (H || 192) || .. ]
-        let a3 = allocator.alloc::<[u8; 192]>().unwrap();
+        let a3 = allocator.allocate::<[u8; 192]>().unwrap();
         assert_eq!(
             a3.as_ptr() as usize,
             a2.as_ptr() as usize + 128 + size_of::<Header>()
@@ -128,21 +167,21 @@ mod tests {
         allocator.free(a2).unwrap();
 
         // [ (H || 64) || (H || 24) || (H || Free(104)) || (H || 192) || .. ]
-        let a5 = allocator.alloc::<[u8; 24]>().unwrap();
+        let a5 = allocator.allocate::<[u8; 24]>().unwrap();
         assert_eq!(
             a5.as_ptr() as usize,
             a1.as_ptr() as usize + 64 + size_of::<Header>()
         );
 
         // [ (H || 64) || (H || 24) || || (H || 32) || (H || Free(72)) || (H || 192) || .. ]
-        let a6 = allocator.alloc::<[u8; 32]>().unwrap();
+        let a6 = allocator.allocate::<[u8; 32]>().unwrap();
         assert_eq!(
             a6.as_ptr() as usize,
             a5.as_ptr() as usize + 24 + size_of::<Header>()
         );
 
         // [ (H || 64) || (H || 24) || || (H || 32) || (H || Free(72)) || (H || 192) || (H || 96) ]
-        let a7 = allocator.alloc::<[u8; 96]>().unwrap();
+        let a7 = allocator.allocate::<[u8; 96]>().unwrap();
         assert_eq!(
             a7.as_ptr() as usize,
             a3.as_ptr() as usize + 192 + size_of::<Header>()
