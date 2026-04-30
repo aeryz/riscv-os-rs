76e71585c6605e12e6b836cb570151050bfa53fb
kernelnomad <kernelnomad@proton.me>
2026-04-30 21:31:13 +0300

    fix task table realloc bug
    Signed-off-by: kernelnomad <kernelnomad@proton.me>

--- kernel/src/task/table.rs
+++ kernel/src/task/table.rs
@@ -7,6 +7,14 @@ use crate::task::Task;
 
 static TASK_TABLE: TaskTable = TaskTable(SpinLock::new(Vec::new()));
 
+// TODO(aeryz): this is super crucial right now since we can't handle task table
+// getting realloc'ed. However, this is just a hack and it's doomed to fail.
+// Until we have a parent/child relationship and convert the tasktable to be a
+// tree, we can keep this.
+pub fn init() {
+    TASK_TABLE.0.lock().reserve(64);
+}
+
 pub fn add_task(task: Task) -> NonNull<Task> {
     let mut table = TASK_TABLE.0.lock();
     let len = table.len();
